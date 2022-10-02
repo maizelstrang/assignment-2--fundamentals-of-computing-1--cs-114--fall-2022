5de521577656ee76b9a7714cce9fb0a38a2a23d1
maizelstrang <mastrang@hartford.edu>
2022-10-02 16:28:34 -0400

    Begin if statement for odd numbered input

--- Assignment2.java
+++ Assignment2.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Assignment2 {
     public static void main(String[] args) {
 
-        System.out.println("Enter a number:");
+        int diamondDimension;
+
+        System.out.print("Enter a number:");
+        diamondDimension = Scanner.nextInt();
+
+        if (0 < diamondDimension && diamondDimension % 2 != 0)
+        {
+
+        }
     }
 }
