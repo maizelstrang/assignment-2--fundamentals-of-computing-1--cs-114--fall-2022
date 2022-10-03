f5d56b2bdb3d6c0dadc0f4286d294ec329804c7d
maizelstrang <mastrang@hartford.edu>
2022-10-03 09:39:46 -0400

    Fix scanner to gain user input

--- Assignment2.java
+++ Assignment2.java
@@ -4,16 +4,17 @@ public class Assignment2 {
     public static void main(String[] args) {
 
         int diamondDimension;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number:");
-        diamondDimension = Scanner.nextInt();
+        System.out.print("Enter a number: ");
+        diamondDimension = scanner.nextInt();
 
         if (0 < diamondDimension && diamondDimension % 2 != 0)
         {
             for (int row = 1; row <= diamondDimension; row++)
             {
-                for (int star = 1; star <= row; )
-                System.out.print("*");
+                for (int star = 1; star <= row; row++)
+                System.out.println("*");
             }
         }
     }
