4bac6c0c40cd27cb98f24bc0994a1bd2a861e158
maizelstrang <mastrang@hartford.edu>
2022-10-05 08:53:01 -0400

    Final

--- Assignment2.java
+++ Assignment2.java
@@ -19,31 +19,22 @@ public class Assignment2 {
                 for (star = 0; star < row * 2 - 1; star++){
                     System.out.print("*");
                 }
-            System.out.println();
+
+                System.out.println();
             }
-
-            // for (row = diamondDimension % 2;)
-
-            /*for (int row = 1; row <= diamondDimension - 1; row ++){
-                for (int spaces = diamondDimension; spaces < row; spaces++){
-                    System.out.println(" ");
-                }
-                for (int star = 0; star > row; star--){
-                    System.out.print("*");
-                }*/
-            }
-
+        }
         else {
             for (row = 1; row <= diamondDimension / 2; row++)
             {
                 for (spaces = diamondDimension; spaces > row; spaces--){
                     System.out.print(" ");
                 }
-                for (star = 0; star < row * 2 - 1; star++){
+                for (star = 0; star < row * 2 - 1; star += 2){
                     System.out.print("* ");
                 }
-            System.out.println();
+
+                System.out.println();
             }
         }
-        }
     }
+}
